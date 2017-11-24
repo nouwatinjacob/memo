8a30605cdad6916905c25adaf3d563aae8708c4b
sansaristic <jaysansa@gmail.com>
2017-11-24 05:30:57 +0100

    header separation

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Memo</title>
+    <link rel="stylesheet" type="text/css" href="{{ url("css/bootstrap.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{ url("css/style.css")}}">
+    <script type="text/javascript" src="{{ url("js/jquery-3.1.0.js")}}"></script>
+    <script type="text/javascript" src="{{ url("js/bootstrap.js")}}"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">REMIND ME</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
